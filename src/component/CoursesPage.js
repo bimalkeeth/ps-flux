35e12eba13db81f0e4eba99c931331e2e957aa9d
Bimal Kaluarachchi <bimalkeeth@gmail.com>
2020-01-15 15:56:45 +1100

    react

---
+++ src/component/CoursesPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+class CoursesPage extends  React.Component{
+    state = {
+      courses:[]
+    };
+
+    componentDidMount() {
+    }
+    render() {
+     return <h2>Courses</h2>
+    }
+}
+export  default CoursesPage
---
+++ src/component/HomePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+function HomePage() {
+  return <div className="jumbotron">
+     <h1>Pluralsight Administration</h1>
+     <p>ertretrterterre</p>
+      <a href="/about">About</a>
+   </div>
+}
+export default HomePage
